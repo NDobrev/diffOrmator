bac9fe50eb52f36ee5c67a1f72878f4250534bab
Martin Dobrev <martindobrev0@gmail.com>
2020-02-20 20:19:03 +0200

    Updates

---
+++ precache-manifest.0fed9c59cf1e7623cfb4f4b07af188ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c423606dd6a65ed2b91080229504e2c5",
+    "url": "/sravnitel/index.html"
+  },
+  {
+    "revision": "d7071bfaabd388960c50",
+    "url": "/sravnitel/static/js/2.2d130b86.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/sravnitel/static/js/2.2d130b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692c15ab39cca6156297",
+    "url": "/sravnitel/static/js/main.8ab81e61.chunk.js"
+  },
+  {
+    "revision": "f4bc12598241efb18ab2",
+    "url": "/sravnitel/static/js/runtime-main.5af97f86.js"
+  }
+]);
